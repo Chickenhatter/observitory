d20a111ed0287e5b756c962aa26a315f5eaeab63
ChickenHatter <b3rr1350bra1n@gmail.com>
2026-05-30 15:04:18 +1200

    6:07 tried adding image and now trying to acess.env

--- src/javascript/look.js
+++ src/javascript/look.js
@@ -34,3 +34,21 @@ function starwalking() {
 }
 
 starwalking();
+
+
+
+
+const key = import.meta.env.API_KEY; 
+async function API_grab() {
+	try {
+		const fim = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`);
+		const tean = await fim.json();
+		const image = (tean.url)
+		document.getElementById('the_image').src = image
+	} catch (error){
+		console.error('Error fetching data:', error);
+		console.log('Image has been added')
+		document.getElementById('the_image').src = 'https://apod.nasa.gov/apod/image/2605/M104noirlab2612a_1024.jpg'
+	}
+}
+API_grab();
