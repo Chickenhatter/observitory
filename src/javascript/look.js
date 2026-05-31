fb887997a12a1c1c309adf656232fe6c18c8c84e
ChickenHatter <b3rr1350bra1n@gmail.com>
2026-05-31 13:44:29 +1200

    7hr 41m trying to make the movement of the screen not go over and added a blur

--- src/javascript/look.js
+++ src/javascript/look.js
@@ -2,8 +2,12 @@ const keys = {};
 const image = document.getElementById('the_image')
 let y = 0;
 let x = 0;
+let t = 0;
 let place_in_spacet = 0
 let place_in_spaced = 0
+let rect = image.getBoundingClientRect();
+let topleft = { x: rect.left,        y: rect.top };
+
 
 document.addEventListener('keydown', (e) => {
     keys[e.key.toLowerCase()] = true
@@ -15,20 +19,40 @@ document.addEventListener('keyup', (e) => {
 function starwalking() {
     if (keys['w']) {
         image.style.top = (y) + 'px'
-        y -= 1
+        if ( y < 0) {
+            y += 1
+        }
     }
     if (keys['s']) {
         image.style.top = (y) + 'px'
-        y += 1
+        y -= 1
     }
     if (keys['a']) {
         image.style.left = (x) + 'px'
-        x -= 1
+        if ( x < 0) {
+            x += 1
+        }
     }
     if (keys['d']) {
         image.style.left = (x) + 'px'
-        x += 1
+        x -= 1
     }
+    if (keys['t']) {
+        t += 0.3
+        image.style.filter = 'blur('+ (Math.abs(t)) +'vw)';
+        console.log(t)
+    }
+    if (keys['g']) {
+        t -= 0.3
+        image.style.filter = 'blur('+ (Math.abs(t)) +'vw)';
+    }
+    rect = image.getBoundingClientRect();
+    topleft = { x: rect.left,        y: rect.top };
+    console.log(topleft)
+    let screenWidth = window.screen.width; 
+    console.log(screenWidth + "px");
+    let screenHeight = window.screen.height; 
+    console.log(screenHeight + "px");
     requestAnimationFrame(starwalking)
 }
 
